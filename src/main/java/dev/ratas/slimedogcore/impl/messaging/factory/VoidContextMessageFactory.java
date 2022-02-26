ba0904c0d75e76f882a3aa38e2a22d9de18463ae
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 18:18:03 +0000

    Moved void message factory to void context message factory

--- src/main/java/dev/ratas/slimedogcore/impl/messaging/factory/VoidContextMessageFactory.java
+++ src/main/java/dev/ratas/slimedogcore/impl/messaging/factory/VoidContextMessageFactory.java
@@ -4,9 +4,9 @@ import dev.ratas.slimedogcore.api.messaging.context.SDCVoidContext;
 import dev.ratas.slimedogcore.api.messaging.context.factory.SDCContextFactory;
 import dev.ratas.slimedogcore.api.messaging.factory.SDCVoidContextMessageFactory;
 
-public class VoidMessageFactory extends MessageFactory<SDCVoidContext> implements SDCVoidContextMessageFactory {
+public class VoidContextMessageFactory extends MessageFactory<SDCVoidContext> implements SDCVoidContextMessageFactory {
 
-    public VoidMessageFactory(Class<?> messageClass, Class<?> contextClass,
+    public VoidContextMessageFactory(Class<?> messageClass, Class<?> contextClass,
             SDCContextFactory<SDCVoidContext> contextFactory, String raw) {
         super(messageClass, contextClass, contextFactory, raw);
     }

159fe3c7258b79ad0ec8118b10fdb32d5ff16d03
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 12:58:23 +0000

    Removed unused interfaces

--- src/main/java/dev/ratas/slimedogcore/api/messaging/delivery/ActionBarMessageDeliverer.java
+++
@@ -1,9 +0,0 @@
-package dev.ratas.slimedogcore.api.messaging.delivery;
-
-public interface ActionBarMessageDeliverer extends MessageDeliverer {
-
-    default MessageTarget getDeliveryTarget() {
-        return MessageTarget.ACTION_BAR;
-    }
-
-}
--- src/main/java/dev/ratas/slimedogcore/api/messaging/delivery/TextMessageDeliverer.java
+++
@@ -1,9 +0,0 @@
-package dev.ratas.slimedogcore.api.messaging.delivery;
-
-public interface TextMessageDeliverer extends MessageDeliverer {
-
-    default MessageTarget getDeliveryTarget() {
-        return MessageTarget.TEXT;
-    }
-
-}

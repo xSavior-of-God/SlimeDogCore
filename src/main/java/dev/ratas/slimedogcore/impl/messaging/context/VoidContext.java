a1e150f2c19ff8b53579f1053141349ee2be72a8
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 17:53:29 +0000

    Added a void context instance to the class

--- src/main/java/dev/ratas/slimedogcore/impl/messaging/context/VoidContext.java
+++ src/main/java/dev/ratas/slimedogcore/impl/messaging/context/VoidContext.java
@@ -3,6 +3,7 @@ package dev.ratas.slimedogcore.impl.messaging.context;
 import dev.ratas.slimedogcore.api.messaging.context.SDCVoidContext;
 
 public class VoidContext implements SDCVoidContext {
+    public static final VoidContext INSTANCE = new VoidContext();
 
     @Override
     public String fill(String msg) {

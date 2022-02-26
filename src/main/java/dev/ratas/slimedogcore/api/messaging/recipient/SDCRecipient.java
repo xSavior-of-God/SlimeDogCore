c9543740a887ed152ee0f53fb58ccb3973a0bf7f
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 22:00:00 +0000

    Added javadocs for recipient

--- src/main/java/dev/ratas/slimedogcore/api/messaging/recipient/SDCRecipient.java
+++ src/main/java/dev/ratas/slimedogcore/api/messaging/recipient/SDCRecipient.java
@@ -3,8 +3,17 @@ package dev.ratas.slimedogcore.api.messaging.recipient;
 import dev.ratas.slimedogcore.api.messaging.SDCMessage;
 import dev.ratas.slimedogcore.api.messaging.context.SDCContext;
 
+/**
+ * The recipient of messages as well as a permission holder.
+ */
 public interface SDCRecipient {
 
+    /**
+     * Send a message to the recipient.
+     *
+     * @param <T>     the type of context for the message
+     * @param message the message in question
+     */
     <T extends SDCContext> void sendMessage(SDCMessage<T> message);
 
     boolean hasPermission(String perms);

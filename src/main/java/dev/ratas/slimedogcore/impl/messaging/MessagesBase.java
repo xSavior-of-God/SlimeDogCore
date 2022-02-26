38568ce164f4a4b1b2e1717c9ee083a1940cca65
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 12:34:36 +0000

    Added a messages base implementation upon which message implementation should be based

---
+++ src/main/java/dev/ratas/slimedogcore/impl/messaging/MessagesBase.java
@@ -0,0 +1,17 @@
+package dev.ratas.slimedogcore.impl.messaging;
+
+import dev.ratas.slimedogcore.api.config.SDCCustomConfig;
+
+public abstract class MessagesBase {
+    private final SDCCustomConfig config;
+
+    protected MessagesBase(SDCCustomConfig config) {
+        this.config = config;
+        config.saveDefaultConfig();
+    }
+
+    protected String getRawMessage(String path, String def) {
+        return config.getConfig().getString(path, def);
+    }
+
+}

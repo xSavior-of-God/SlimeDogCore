1a598a5b0ea5a22c1101e55e8666d2b80d7d677f
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-26 16:56:23 +0000

    Fixing typo in type parameter in extending interfacce

--- src/main/java/dev/ratas/slimedogcore/api/messaging/factory/SDCQuadrupleContextMessageFactory.java
+++ src/main/java/dev/ratas/slimedogcore/api/messaging/factory/SDCQuadrupleContextMessageFactory.java
@@ -7,6 +7,6 @@ import dev.ratas.slimedogcore.api.messaging.context.SDCQuadrupleContext;
  * required inputs.
  */
 public interface SDCQuadrupleContextMessageFactory<T1, T2, T3, T4>
-                extends SDCMessageFactory<SDCQuadrupleContext<T1, T2, T3, T3>> {
+                extends SDCMessageFactory<SDCQuadrupleContext<T1, T2, T3, T4>> {
 
 }

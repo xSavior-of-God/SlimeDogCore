39d1848ec89dc3f91bdaaedc7e9ab904c469c8a3
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-03-02 20:23:06 +0000

    Base settings now able to get the config at a later time (so as to not need it at initialization of core which may be too early)

--- src/main/java/dev/ratas/slimedogcore/impl/config/BaseSettings.java
+++ src/main/java/dev/ratas/slimedogcore/impl/config/BaseSettings.java
@@ -1,17 +1,23 @@
 package dev.ratas.slimedogcore.impl.config;
 
+import java.util.function.Supplier;
+
 import dev.ratas.slimedogcore.api.config.SDCCustomConfig;
 import dev.ratas.slimedogcore.api.config.settings.SDCBaseSettings;
 
 public class BaseSettings implements SDCBaseSettings {
-    private final SDCCustomConfig config;
+    private final Supplier<SDCCustomConfig> configSupplier;
+    private SDCCustomConfig config = null;
 
-    public BaseSettings(SDCCustomConfig config) {
-        this.config = config;
+    public BaseSettings(Supplier<SDCCustomConfig> configSupplier) {
+        this.configSupplier = configSupplier;
     }
 
     @Override
     public boolean isDebugModeEnabled() {
+        if (config == null) {
+            config = configSupplier.get();
+        }
         return config.getConfig().getBoolean("debug", false);
     }
 

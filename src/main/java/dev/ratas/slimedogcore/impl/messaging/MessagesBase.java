b6ccc89ba090e9e6344070e790f794151ebe9299
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-07-03 19:53:55 +0100

    Message base now has shorthands to fill placeholders in messages

--- src/main/java/dev/ratas/slimedogcore/impl/messaging/MessagesBase.java
+++ src/main/java/dev/ratas/slimedogcore/impl/messaging/MessagesBase.java
@@ -1,6 +1,15 @@
 package dev.ratas.slimedogcore.impl.messaging;
 
 import dev.ratas.slimedogcore.api.config.SDCCustomConfig;
+import dev.ratas.slimedogcore.api.messaging.SDCMessage;
+import dev.ratas.slimedogcore.api.messaging.context.SDCDoubleContext;
+import dev.ratas.slimedogcore.api.messaging.context.SDCQuadrupleContext;
+import dev.ratas.slimedogcore.api.messaging.context.SDCSingleContext;
+import dev.ratas.slimedogcore.api.messaging.context.SDCTripleContext;
+import dev.ratas.slimedogcore.api.messaging.factory.SDCDoubleContextMessageFactory;
+import dev.ratas.slimedogcore.api.messaging.factory.SDCQuadrupleContextMessageFactory;
+import dev.ratas.slimedogcore.api.messaging.factory.SDCSingleContextMessageFactory;
+import dev.ratas.slimedogcore.api.messaging.factory.SDCTripleContextMessageFactory;
 import dev.ratas.slimedogcore.api.reload.ReloadException;
 import dev.ratas.slimedogcore.api.reload.SDCReloadable;
 
@@ -35,6 +44,25 @@ public abstract class MessagesBase implements SDCReloadable {
     @Override
     public void reload() throws ReloadException {
         config.reload();
+    }
+
+    public <T> SDCMessage<SDCSingleContext<T>> fill(SDCSingleContextMessageFactory<T> factory, T t) {
+        return factory.getMessage(factory.getContextFactory().getContext(t));
+    }
+
+    public <T1, T2> SDCMessage<SDCDoubleContext<T1, T2>> fill(SDCDoubleContextMessageFactory<T1, T2> factory, T1 t1,
+            T2 t2) {
+        return factory.getMessage(factory.getContextFactory().getContext(t1, t2));
+    }
+
+    public <T1, T2, T3> SDCMessage<SDCTripleContext<T1, T2, T3>> fill(
+            SDCTripleContextMessageFactory<T1, T2, T3> factory, T1 t1, T2 t2, T3 t3) {
+        return factory.getMessage(factory.getContextFactory().getContext(t1, t2, t3));
+    }
+
+    public <T1, T2, T3, T4> SDCMessage<SDCQuadrupleContext<T1, T2, T3, T4>> fill(
+            SDCQuadrupleContextMessageFactory<T1, T2, T3, T4> factory, T1 t1, T2 t2, T3 t3, T4 t4) {
+        return factory.getMessage(factory.getContextFactory().getContext(t1, t2, t3, t4));
     }
 
 }

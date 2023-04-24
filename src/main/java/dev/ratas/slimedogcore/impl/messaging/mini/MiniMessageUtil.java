6ff7f02d0da9865ebfbbaa02018f387104cf56d0
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2023-04-24 21:42:08 +0100

    Better minimessage detection (with escape character)

--- src/main/java/dev/ratas/slimedogcore/impl/messaging/mini/MiniMessageUtil.java
+++ src/main/java/dev/ratas/slimedogcore/impl/messaging/mini/MiniMessageUtil.java
@@ -1,8 +1,12 @@
 package dev.ratas.slimedogcore.impl.messaging.mini;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class MiniMessageUtil {
+    private static final Pattern LEFT_ANGLE_BRACKET = Pattern.compile("(?<=\\)b<");
+    private static final Pattern RIGHT_ANGLE_BRACKET = Pattern.compile("(?<=\\)b>");
 
     private MiniMessageUtil() {
         // prviate constructor
@@ -10,16 +14,23 @@ public final class MiniMessageUtil {
 
     public static boolean textCouldBeMiniMessage(String raw) {
         // if it doesn't contain angle brackets, clearly not a mini-message
-        if (!raw.contains("<")) {
+        Matcher leftMatcher = LEFT_ANGLE_BRACKET.matcher(raw);
+        if (!leftMatcher.find()) {
             return false;
         }
         // if it has a different number of start and end brackets, not a
         // (correctly formated) mini-message
-        if (StringUtils.countMatches(raw, "<") != StringUtils.countMatches(raw, ">")) {
+        Matcher rightMatcher = RIGHT_ANGLE_BRACKET.matcher(raw);
+        // 1 already counted above with #find
+        long totalLeftMatches = leftMatcher.results().count() + 1;
+        long totalRightMatches = rightMatcher.results().count();
+        if (totalLeftMatches != totalRightMatches) {
             return false;
         }
         // if the first < is after the first >, it's _probably_ not a minimessage
-        if (raw.indexOf("<") > raw.indexOf(">")) {
+        MatchResult firstLeft = leftMatcher.reset().results().findFirst().get();
+        MatchResult firstRight = rightMatcher.reset().results().findFirst().get();
+        if (firstLeft.start() > firstRight.start()) {
             return false;
         }
         return true;
